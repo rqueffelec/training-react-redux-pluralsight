793b2b7526e03e737f8132dc872336d8402e002f
Regis Queffelec <rqueffelec@dxc.com>
2019-07-08 16:09:42 +0930

    Added all courses actions

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,23 +1,61 @@
 import * as types from "./actionTypes";
 import * as courseApi from "../../api/courseApi";
-
-export function createCourse(course) {
-  return { type: types.CREATE_COURSE, course };
-}
+import { beginApiCall, apiCallError } from "./aipStatusActions";
 
 export function loadCoursesSuccess(courses) {
   return { type: types.LOAD_COURSES_SUCCESS, courses };
 }
 
+export function createCourseSuccess(course) {
+  return { type: types.CREATE_COURSE_SUCCESS, course };
+}
+
+export function updateCourseSuccess(course) {
+  return { type: types.UPDATE_COURSE_SUCCESS, course };
+}
+
+export function deleteCourseOptimistic(course) {
+  return { type: types.DELETE_COURSE_OPTIMISTIC, course };
+}
+
 export function loadCourses() {
   return function(dispatch) {
+    dispatch(beginApiCall());
     return courseApi
       .getCourses()
       .then(courses => {
         dispatch(loadCoursesSuccess(courses));
       })
       .catch(error => {
+        dispatch(apiCallError(error));
         throw error;
       });
+  };
+}
+
+export function saveCourse(course) {
+  //es-lint-disable-next-line no-unused-vars
+  return function(dispatch) {
+    dispatch(beginApiCall());
+    return courseApi
+      .saveCourse(course)
+      .then(savedCourse => {
+        course.id
+          ? dispatch(updateCourseSuccess(saveCourse))
+          : dispatch(createCourseSuccess(savedCourse));
+      })
+      .catch(error => {
+        dispatch(apiCallError(error));
+        throw error;
+      });
+  };
+}
+
+export function deleteCourse(course) {
+  return function(dispatch) {
+    // Doing optimistic delete, so not dispatching begin/end api call
+    // actions, or apiCallError action since we're not showing the loading status of this.
+    dispatch(deleteCourseOptimistic(course));
+    return courseApi.deleteCourse(course.id);
   };
 }
