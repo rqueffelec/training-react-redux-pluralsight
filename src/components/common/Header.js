2e308bdf3718552a6a48fcb051e43a105bafa17b
Regis Queffelec <rqueffelec@dxc.com>
2019-07-19 13:12:09 +0930

    Cory's challenge: Show items number in the header.
    - Updated the header to have access to the list of authors and courses to display the number of items available

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavLink } from "react-router-dom";
+import { connect } from "react-redux";
+import { loadCourses } from "../../redux/actions/courseActions";
+import { loadAuthors } from "../../redux/actions/authorActions";
+import PropTypes from "prop-types";
 
 /*
 Cory's challenge: Author administration
 Added an Authors navigation link
 */
-const Header = () => {
+/*
+Cory's challenge: Show items number in the header.
+Updated the Header component to get all required information 
+from the store and display the items number with the matching 
+link for the user to view.
+Note: this has highlighted the requirement to centralize the 
+items load in a common place when the entire application loads.
+Instead of doing it in every component.
+I would suggest to do that in the App.js. 
+*/
+const Header = ({ courses, authors, loadCourses, loadAuthors }) => {
   const activeStyle = { color: "#F15B2A" };
+
+  useEffect(() => {
+    if (courses.length === 0) {
+      loadCourses().catch(error => {
+        alert("Loading courses failed " + error);
+      });
+    }
+
+    if (authors.length === 0) {
+      loadAuthors().catch(error => {
+        alert("Loading authors failed " + error);
+      });
+    }
+  }, []);
 
   return (
     <nav>
@@ -15,11 +43,11 @@ const Header = () => {
       </NavLink>
       {" | "}
       <NavLink to="/courses" activeStyle={activeStyle}>
-        Courses
+        Courses {courses.length === 0 ? "" : "(" + courses.length + ")"}
       </NavLink>
       {" | "}
       <NavLink to="/authors" activeStyle={activeStyle}>
-        Authors
+        Authors {authors.length === 0 ? "" : "(" + authors.length + ")"}
       </NavLink>
       {" | "}
       <NavLink to="/about" activeStyle={activeStyle}>
@@ -29,4 +57,26 @@ const Header = () => {
   );
 };
 
-export default Header;
+Header.propTypes = {
+  courses: PropTypes.array,
+  authors: PropTypes.array,
+  loadCourses: PropTypes.func.isRequired,
+  loadAuthors: PropTypes.func.isRequired
+};
+
+function mapStateToProps(state) {
+  return {
+    courses: state.courses,
+    authors: state.authors
+  };
+}
+
+const mapDispatchToProps = {
+  loadCourses,
+  loadAuthors
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Header);
